0da29d2986fe3ad0722c5cc8872c426664af963d
Daweihao <421679009@qq.com>
2019-02-16 16:32:11 -0500

    1 c Completed

--- hw1/index.js
+++ hw1/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+let myObj = {Name:'Haoran'};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.post('/show', function (req, res) {
+  // req.on('data', function (chunk) {
+  //   content += chunk;
+  // });
+  // req.on('end', function (){
+  //   res.writeHead(200, {"Content-Type":"x-www-form-urlencoded"});
+  //   res.write("You've sent:  "+ content);
+  //   res.end();
+  // }
+  // );
+  res.send(myObj);
 });
 module.exports = router;
--- hw1/users.js
+++ hw1/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 let myObj = {name:"Haoran Wei"};
-let myJSON = JSON.stringify(myObj);
+// method 1 Transforming obj to JSON: let myJSON = JSON.stringify(myObj);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(myJSON);
+  // res.send(myJSON);
+  // method 2 use res.json() to send obj
+  res.json(myObj);
 });
+
+
 
 module.exports = router;
