f9cfaae52796086fa7bd4de567fb054e3f73bcf6
Daweihao <421679009@qq.com>
2019-02-16 15:39:00 -0500

    1 (b)

--- hw1/users.js
+++ hw1/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+let myObj = {name:"Haoran Wei"};
+let myJSON = JSON.stringify(myObj);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(myJSON);
 });
 
 module.exports = router;
