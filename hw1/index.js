6bd1a2fcee9b59637b5b851fbd5c343b303ad27c
Daweihao <421679009@qq.com>
2019-02-18 14:50:46 -0500

    Final version.

--- hw1/index.js
+++ hw1/index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var router = express.Router();
-let myObj = {Name:'Haoran'};
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 router.post('/show', function (req, res) {
-  // req.on('data', function (chunk) {
-  //   content += chunk;
-  // });
-  // req.on('end', function (){
-  //   res.writeHead(200, {"Content-Type":"x-www-form-urlencoded"});
-  //   res.write("You've sent:  "+ content);
-  //   res.end();
-  // }
-  // );
+    // post will return the body message wrapped in request.
+  let myObj = {message:req.body.message};
   res.send(myObj);
 });
 module.exports = router;
